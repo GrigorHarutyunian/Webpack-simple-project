b825f70803461055a0bcbeda98f23c0406809637
Grigor Harutyunyan <grigorharutyunian97@gmail.com>
2023-11-26 00:20:50 +0400

    add image gallery for About Us section and add this style

--- src/about-us/createAboutUsSection.js
+++ src/about-us/createAboutUsSection.js
@@ -1,58 +1,57 @@
-export function createAboutUsSection() {
-  const aboutUsSection = document.createElement("section");
+import img1 from "./images/starfish.jpg";
+import img2 from "./images/most-endangered-marine-species.jpg";
+import img3 from "./images/marine-animals-sea-turtle-1022.jpg";
+import img4 from "./images/lionfish.jpg";
+import img5 from "./images/endangered-hawaiian-monk-seal.jpg";
+import img6 from "./images/coral-reef_2021-06-08.jpg";
+import img7 from "./images/B9SfAgAmL668nUeUfL5KI.jpeg";
+import img8 from "./images/8c8db5e4e36a473591d1b59844fd1a64.jpeg";
+
+export function createAboutUsSection({ element, className }) {
+  const aboutUsSection = document.createElement(element);
   aboutUsSection.setAttribute("id", "aboutUs");
-  aboutUsSection.setAttribute("class", "about_us_section");
-
-  aboutUsSection.style.backgroundColor = "rgba(254, 199, 99, 0.8)";
-
-  const aboutUsContent = document.createElement("div");
-  aboutUsContent.setAttribute("class", "about_us_content");
-
-  const aboutUsTitle = document.createElement("h2");
-  aboutUsTitle.innerText = "About Us";
-  aboutUsTitle.classList.add("about_us_title");
-
-  const aboutUsText = document.createElement("p");
-  aboutUsText.innerText =
-    "Welcome to Aquascape! We are a team of passionate individuals dedicated to creating beautiful aquatic environments. With a commitment to sustainability and education, we aim to inspire and support hobbyists worldwide. Explore our site for expert tips, high-quality products, and a thriving community.";
-  aboutUsText.classList.add("about_us_text");
-
-  const additionalText = document.createElement("p");
-  additionalText.innerText =
-    "Join us on this journey as we dive into the fascinating world of aquascaping and aquarium care. Whether you are a beginner or an experienced hobbyist, we have the resources and products to enhance your aquatic experience.";
-
-  const additionalSentences = document.createElement("p");
-  additionalSentences.innerText =
-    "Our mission is to foster a deep connection between people and nature through the art of aquascaping. As advocates for environmental stewardship, we promote responsible practices that contribute to the well-being of aquatic life. Join us in our mission to create stunning underwater landscapes that captivate the imagination.";
-
-  const evenMoreSentences = document.createElement("p");
-  evenMoreSentences.innerText =
-    "At Aquascape, we believe that everyone, regardless of experience, can enjoy the wonders of aquariums. Whether you dream of a serene freshwater setup or a vibrant reef aquarium, we provide the resources and guidance to turn your vision into reality. Embark on a journey of discovery with Aquascape!";
-
-  aboutUsContent.appendChild(aboutUsTitle);
-  aboutUsContent.appendChild(aboutUsText);
-  aboutUsContent.appendChild(additionalText);
-  aboutUsContent.appendChild(additionalSentences);
-  aboutUsContent.appendChild(evenMoreSentences);
-  aboutUsSection.appendChild(aboutUsContent);
-
-  aboutUsSection.addEventListener("mouseover", function () {
-    aboutUsSection.style.backgroundColor = "rgba(52, 152, 219, 0.8)";
-    aboutUsTitle.style.transform = "translateY(-3px)";
-    aboutUsText.style.transform = "translateY(-4px)";
-    additionalText.style.transform = "translateY(-4px)";
-    additionalSentences.style.transform = "translateY(-4px)";
-    evenMoreSentences.style.transform = "translateY(-4px)";
-  });
-
-  aboutUsSection.addEventListener("mouseout", function () {
-    aboutUsSection.style.backgroundColor = "rgba(254, 199, 99, 0.8)";
-    aboutUsTitle.style.transform = "translateY(0)";
-    aboutUsText.style.transform = "translateY(0)";
-    additionalText.style.transform = "translateY(0)";
-    additionalSentences.style.transform = "translateY(0)";
-    evenMoreSentences.style.transform = "translateY(0)";
-  });
-
+  aboutUsSection.setAttribute("class", className);
+  aboutUsSection.append(createBox(), createButtons());
   return aboutUsSection;
+}
+
+function createBox() {
+  const imgUrl = [img1, img2, img3, img4, img5, img6, img7, img8];
+  const box = document.createElement("div");
+  box.setAttribute("class", "box");
+  for (let x = 0; x < 9; x++) {
+    const span = document.createElement("span");
+    span.style = "--i:" + (x + 1);
+    const img = document.createElement("img");
+    img.src = imgUrl[x];
+    span.append(img);
+    box.append(span);
+  }
+  box;
+  return box;
+}
+
+function createButtons() {
+  const btns = document.createElement("div");
+  btns.setAttribute("class", "btns");
+  const btnPrev = document.createElement("div");
+  btnPrev.setAttribute("class", "btn prev");
+  const btnNext = document.createElement("div");
+  btnNext.setAttribute("class", "btn next");
+  btns.append(btnPrev, btnNext);
+  let degrees = 0;
+  const box = document.querySelector(".box");
+  btnPrev.addEventListener("click", () => {
+    const box = document.querySelector(".box");
+    degrees += 60;
+    box.style = `transform:perspective(1150px) rotateY(${degrees}deg)`;
+  });
+
+  btnNext.addEventListener("click", () => {
+    const box = document.querySelector(".box");
+    degrees -= 60;
+    box.style = `transform:perspective(1150px) rotateY(${degrees}deg)`;
+  });
+
+  return btns;
 }
