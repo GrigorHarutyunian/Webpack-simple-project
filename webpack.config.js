0c87f24f0fc202805f988cb4cf1a590a90f38132
Mnae Vardazaryan <manevardazaryan@gmail.com>
2023-12-01 04:24:46 -0800

    Fix css

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const autoprefixer = require('autoprefixer');
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
@@ -10,36 +11,67 @@ module.exports = {
     open: true,
     historyApiFallback: true,
   },
+
   entry: {
     main: path.resolve(__dirname, "src/index.js"),
   },
+
   output: {
     filename: "[name].js",
     path: path.resolve(__dirname, "dist"),
   },
+
   plugins: [
     new HTMLWebpackPlugin({
       template: path.resolve(__dirname, "src/index.html"),
     }),
     new CleanWebpackPlugin(),
   ],
+
   module: {
     rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+
       {
         test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
         type: "asset/resource",
       },
+
       {
         test: /\.webp$/i,
         type: "asset",
       },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
+
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: "asset/resource",
+      },
+
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  autoprefixer(),
+                ],
+              },
+            },
+          },
+        ],
       },
     ],
   },
